7b0c69baaf5e1910bf0cb08b258f321440389b57
Rickiwasho <ricardo.coronado@alumnos.uach.cl>
2021-03-05 14:31:40 -0300

    test unitario

---
+++ actividades/4.pytest/main.py
@@ -0,0 +1,15 @@
+
+def suma(x, y):
+
+    '''
+
+        Funcion que toma dos argumentos y devuelve la suma de los mismos
+
+        Args:
+            x (int o float): Primer valor del sumando.
+            y (int o float): Segundo valor del sumando.
+        
+        Return: x+y
+
+    '''
+    return x+y
